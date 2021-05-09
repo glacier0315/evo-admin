22fadf1c5d6dee1dc6a70e4625a8d417ca0738e9
glacier <glacier0315@126.com>
2021-05-09 15:58:07 +0800

    修改代码,集成AdminLTE-3.1.0

---
+++ evo-web-adapter/src/main/java/com/glacier/evo/web/sys/IndexController.java
@@ -0,0 +1,40 @@
+package com.glacier.evo.web.sys;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 2017/4/15 0015
+ *
+ * @author glacier
+ * @version v1.0.0
+ */
+@Controller
+public class IndexController {
+	
+	@GetMapping("")
+	public String index(Model model) {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard(Model model) {
+		return "pages/dashboard/index";
+	}
+	
+	@GetMapping("/dashboard2")
+	public String dashboard2(Model model) {
+		return "pages/dashboard/index2";
+	}
+	
+	@GetMapping("/dashboard3")
+	public String dashboard3(Model model) {
+		return "pages/dashboard/index3";
+	}
+}
