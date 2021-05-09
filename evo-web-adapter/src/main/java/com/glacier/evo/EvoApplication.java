dbfdb4ae24a9178559e4a08b6533180eaa103cd5
glacier <glacier0315@126.com>
2021-05-09 15:58:07 +0800

    集成Pear-Admin-Layui

---
+++ evo-web-adapter/src/main/java/com/glacier/evo/EvoApplication.java
@@ -0,0 +1,18 @@
+package com.glacier.evo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * date 2021-05-09 15:21
+ *
+ * @author glacier
+ * @version 1.0
+ */
+@SpringBootApplication
+public class EvoApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EvoApplication.class, args);
+    }
+}
